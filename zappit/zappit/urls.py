3d753f3a76a330dbbc9c310f40bc3a5828b4c898
nanervah <nanervah@yandex.ru>
2022-01-12 23:13:09 +0700

    #3

--- zappit/zappit/urls.py
+++ zappit/zappit/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/posts', views.PostList.as_view()),
+    path('api/posts/<int:pk>/vote', views.VoteCreate.as_view()),
 ]
